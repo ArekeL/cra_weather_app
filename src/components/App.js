9e05c98a0e1361782d57a79d7e5fbfe6250e1cce
Arek Lechnik <alechnik@gmail.com>
2022-12-19 06:35:07 +0100

    First push

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+	state = {};
+	render() {
+		return (
+			<>
+				<h1 className='App'>Hello Word</h1>
+			</>
+		);
+	}
+}
+
+export default App;
